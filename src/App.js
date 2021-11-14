74f5290b645310265c550c27d750a72594fbf6cd
guvi-b24we <94277534+guvi-b24we@users.noreply.github.com>
2021-11-14 11:08:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to Guvi React App - B24WE</p>
+        <a
+          className="App-link"
+          href="https://www.guvi.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React from Guvi
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
